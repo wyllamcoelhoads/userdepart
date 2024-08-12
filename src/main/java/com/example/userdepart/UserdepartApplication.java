a329ffec1b324cd9c50c85679a8c764ee6d5b690
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-08-12 12:25:28 -0300

    iniciando

---
+++ src/main/java/com/example/userdepart/UserdepartApplication.java
@@ -0,0 +1,13 @@
+package com.example.userdepart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserdepartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserdepartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/userdepart/UserdepartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userdepart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserdepartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
