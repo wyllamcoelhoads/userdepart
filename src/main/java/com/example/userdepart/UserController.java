64b0ffa6963affd0411b93c67407c91bc4a5e441
wyllamcoelhoads <wyllamcoelho8@gmail.com>
2024-08-13 18:19:40 -0300

    Finalizando o projeto de construir uma api simples

--- src/main/java/com/example/userdepart/UserController.java
+++ src/main/java/com/example/userdepart/UserController.java
@@ -1,9 +1,7 @@
 package com.example.userdepart;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -16,6 +14,17 @@ public class UserController {
     @GetMapping
     public List<User> findAll(){
         List<User> result = repository.findAll();
+        return result;
+    }
+    @GetMapping(value = "/{id}")
+    public User findById(@PathVariable Long id){
+        User result = repository.findById(id).get();
+        return result;
+    }
+
+    @PostMapping
+    public User insert(@RequestBody User user){
+        User result = repository.save(user);
         return result;
     }
 }
